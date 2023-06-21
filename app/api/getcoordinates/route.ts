11d230f99f500fb5b12b9fbce27f3fbe74bd6b1e
jackgf5 <jackgattfloridia7@gmail.com>
2023-06-21 12:30:21 +0100

    add co ordinates

---
+++ app/api/getcoordinates/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server"
+import { formatISO } from "date-fns"
+
+import prisma from "@/lib/prisma"
+
+export async function POST(request: Request) {
+  const body = await request.json()
+  const { companyName } = body
+
+  if (!companyName)
+    return NextResponse.json({ msg: "Missing Fields" }, { status: 400 })
+
+  const organization = await prisma.organization.findFirst({
+    where: {
+      name: companyName,
+    },
+    include: { coordinates: true },
+  })
+
+  if (organization)
+    return NextResponse.json(
+      { msg: `${organization.name} Info`, organization },
+      { status: 200 }
+    )
+
+  const educational = await prisma.educational.findFirst({
+    where: {
+      name: companyName,
+    },
+    include: { coordinates: true },
+  })
+
+  if (educational)
+    return NextResponse.json(
+      { msg: `${educational.name} Info`, educational },
+      { status: 200 }
+    )
+
+  return NextResponse.json({ msg: "No Info Found" }, { status: 404 })
+}
