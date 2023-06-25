098bffce964586184f332c5597f0b4cc38deb138
jackgf5 <jackgattfloridia7@gmail.com>
2023-06-26 00:38:40 +0100

    try fix

--- app/api/employee/checkin/route.ts
+++ app/api/employee/checkin/route.ts
@@ -5,7 +5,7 @@ import prisma from "@/lib/prisma"
 
 export async function POST(request: Request) {
   const body = await request.json()
-  const { userId, now } = body
+  const { userId, isoString } = body
 
   if (!userId)
     return NextResponse.json({ msg: "Missing Fields" }, { status: 400 })
@@ -13,8 +13,8 @@ export async function POST(request: Request) {
   const shift = await prisma.shift.create({
     data: {
       userId: userId,
-      date: formatISO(now),
-      checkinTime: formatISO(now),
+      date: isoString,
+      checkinTime: isoString,
     },
   })
 
