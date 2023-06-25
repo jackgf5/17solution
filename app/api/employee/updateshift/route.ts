6722ec7d7991f87fe85abbf80d52aeeb54f24b4a
jackgf5 <jackgattfloridia7@gmail.com>
2023-06-25 22:49:45 +0100

    client only date fix

--- app/api/employee/updateshift/route.ts
+++ app/api/employee/updateshift/route.ts
@@ -6,21 +6,21 @@ import prisma from "@/lib/prisma"
 
 export async function POST(request: Request) {
   const body = await request.json()
-  const { shiftId, checkinTime } = body
+  const { shiftId, checkinTime, checkoutTime } = body
 
   if (!shiftId)
     return NextResponse.json({ msg: "Missing Fields" }, { status: 400 })
 
-  const date = new Date()
   const start = parseISO(checkinTime)
-  const minutesDifference = differenceInMinutes(date, start).toString()
+  const end = parseISO(checkoutTime)
+  const minutesDifference = differenceInMinutes(end, start).toString()
 
   const updatedShift = await prisma.shift.update({
     where: {
       id: shiftId,
     },
     data: {
-      checkoutTime: formatISO(date),
+      checkoutTime: formatISO(end),
       completed: true,
       durationWorked: minutesDifference,
     },
