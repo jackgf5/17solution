8084508a22445513910bca482f1dff741dff361b
jackgf5 <jackgattfloridia7@gmail.com>
2023-07-04 04:17:05 +0530

    check for conflicting leave application

--- app/api/employee/applyleave/route.ts
+++ app/api/employee/applyleave/route.ts
@@ -9,6 +9,40 @@ export async function POST(request: Request) {
   if (!userId || !startDate || !endDate || !daysOff || !reason || !userId)
     return NextResponse.json({ msg: "Missing Fields" }, { status: 400 })
 
+  const allUsersApplications = await prisma.leaveApplication.findMany({
+    where: {
+      userId: userId,
+    },
+  })
+
+  // Check for date conflicts
+  const conflictingApplication = allUsersApplications.find((application) => {
+    const applicationStartDate = new Date(application.startDate)
+    const applicationEndDate = new Date(application.endDate)
+    const inputStartDate = new Date(startDate)
+    const inputEndDate = new Date(endDate)
+
+    // Check if the date ranges overlap
+    if (
+      (inputStartDate >= applicationStartDate &&
+        inputStartDate <= applicationEndDate) ||
+      (inputEndDate >= applicationStartDate &&
+        inputEndDate <= applicationEndDate) ||
+      (inputStartDate <= applicationStartDate &&
+        inputEndDate >= applicationEndDate)
+    ) {
+      return true // Conflicting dates found
+    }
+    return false // No conflicts
+  })
+
+  if (conflictingApplication) {
+    return NextResponse.json(
+      { msg: "Date conflict with existing application" },
+      { status: 400 }
+    )
+  }
+
   const leaveApplication = await prisma.leaveApplication.create({
     data: {
       startDate,
